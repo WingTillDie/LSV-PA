9df2290f14f705b887a8c697e170d5d40cc5658c
WingTillDie <mychat.01a2bc3def@gmail.com>
2020-10-31 10:40:24 +0800

    Finished dummy function of lsv_print_sopunate

---
+++ src/ext-lsv/lsvPrintSopunate.cpp
@@ -0,0 +1,63 @@
+#include "base/abc/abc.h"
+#include "base/main/main.h"
+#include "base/main/mainInt.h"
+
+static int Lsv_CommandPrintSopunate(Abc_Frame_t* pAbc, int argc, char** argv);
+
+void Lsv_InitPrintSopunate(Abc_Frame_t* pAbc) {
+  Cmd_CommandAdd(pAbc, "LSV", "lsv_print_sopunate", Lsv_CommandPrintSopunate, 0);
+}
+
+void Lsv_DestroyPrintSopunate(Abc_Frame_t* pAbc) {}
+
+Abc_FrameInitializer_t Lsv_FrameInitPrintSopunate = {Lsv_InitPrintSopunate, Lsv_DestroyPrintSopunate};
+
+/*
+struct PackageRegistrationManager {
+  PackageRegistrationManager() { Abc_FrameAddInitializer(&Lsv_FrameInitPrintSopunate); }
+} lsvPackageRegistrationManager;
+*/
+
+void Lsv_NtkPrintSopunate(Abc_Ntk_t* pNtk) {
+  Abc_Obj_t* pObj;
+  int i;
+  Abc_NtkForEachNode(pNtk, pObj, i) {
+    printf("Object Id = %d, name = %s\n", Abc_ObjId(pObj), Abc_ObjName(pObj));
+    Abc_Obj_t* pFanin;
+    int j;
+    Abc_ObjForEachFanin(pObj, pFanin, j) {
+      printf("  Fanin-%d: Id = %d, name = %s\n", j, Abc_ObjId(pFanin),
+             Abc_ObjName(pFanin));
+    }
+    if (Abc_NtkHasSop(pNtk)) {
+      printf("The SOP of this node:\n%s", (char*)pObj->pData);
+    }
+  }
+}
+
+int Lsv_CommandPrintSopunate(Abc_Frame_t* pAbc, int argc, char** argv) {
+  Abc_Ntk_t* pNtk = Abc_FrameReadNtk(pAbc);
+  int c;
+  Extra_UtilGetoptReset();
+  while ((c = Extra_UtilGetopt(argc, argv, "h")) != EOF) {
+    switch (c) {
+      case 'h':
+        goto usage;
+      default:
+        goto usage;
+    }
+  }
+  if (!pNtk) {
+    Abc_Print(-1, "Empty network.\n");
+    return 1;
+  }
+  Lsv_NtkPrintSopunate(pNtk);
+  return 0;
+
+usage:
+  Abc_Print(-2, "usage: lsv_print_sopunate [-h]\n");
+  //Abc_Print(-2, "\t        prints the nodes in the network\n");
+  Abc_Print(-2, "\t        prints sopunate for each node in the network\n");
+  Abc_Print(-2, "\t-h    : print the command usage\n");
+  return 1;
+}
